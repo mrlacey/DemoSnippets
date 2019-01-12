eab0a14010c47b4144c2efbe1a76d852131b2414
Matt Lacey <matt@mrlacey.co.uk>
2019-01-12 14:15:20 +0000

    Add output pane - for custom logging

---
+++ DemoSnippets/GeneralOutputPane.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace DemoSnippets
+{
+    public class OutputPane
+    {
+        private static Guid dsPaneGuid = new Guid("DF45715C-F87B-4B53-B926-BB8C30C7F804");
+
+        private static OutputPane instance;
+
+        private readonly IVsOutputWindowPane pane;
+
+        private OutputPane()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (ServiceProvider.GlobalProvider.GetService(typeof(SVsOutputWindow)) is IVsOutputWindow outWindow
+                && (ErrorHandler.Failed(outWindow.GetPane(ref dsPaneGuid, out this.pane)) || this.pane == null))
+            {
+                if (ErrorHandler.Failed(outWindow.CreatePane(ref dsPaneGuid, "Demo Snippets", 1, 0)))
+                {
+                    System.Diagnostics.Debug.WriteLine("Failed to create output pane.");
+                    return;
+                }
+
+                if (ErrorHandler.Failed(outWindow.GetPane(ref dsPaneGuid, out this.pane)) || (this.pane == null))
+                {
+                    System.Diagnostics.Debug.WriteLine("Failed to get output pane.");
+                }
+            }
+        }
+
+        public static OutputPane Instance => instance ?? (instance = new OutputPane());
+
+        public async Task ActivateAsync()
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+            this.pane?.Activate();
+        }
+
+        public async Task WriteAsync(string message)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+            this.pane?.OutputString($"{message}{Environment.NewLine}");
+        }
+    }
+}
