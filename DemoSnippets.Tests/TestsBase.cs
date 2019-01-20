15a0693763343cf8756a10638b2ede72cd53eac8
Matt Lacey <matt@mrlacey.co.uk>
2019-01-20 14:08:08 +0000

    remove test reliance on Environment.NewLine == \r\n

--- DemoSnippets.Tests/TestsBase.cs
+++ DemoSnippets.Tests/TestsBase.cs
@@ -9,8 +9,29 @@ namespace DemoSnippets.Tests
     {
         internal List<ToolboxEntry> ParseAsLines(string lines)
         {
+            const string crlf = "\r\n";
+            const string cr = "\r";
+
+            var splitString = Environment.NewLine;
+
+            if (lines.Contains(crlf))
+            {
+                if (Environment.NewLine != crlf)
+                {
+                    splitString = crlf;
+                }
+            }
+            else if (lines.Contains(cr))
+            {
+                if (Environment.NewLine != cr)
+                {
+                    splitString = cr;
+                }
+            }
+
             var sut = new DemoSnippetsParser();
-            return sut.GetItemsToAdd(lines.Split(new[] { Environment.NewLine }, StringSplitOptions.None));
+
+            return sut.GetItemsToAdd(lines.Split(new[] { splitString }, StringSplitOptions.None));
         }
 
         internal List<ToolboxEntry> ParseExampleFile(string fileName)
