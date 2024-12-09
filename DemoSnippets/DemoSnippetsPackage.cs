f39753948b190e4e77a448b0ca7ef288194de007
Matt Lacey <matt@mrlacey.co.uk>
2024-12-09 08:44:06 +0000

    add additional analytics data

--- DemoSnippets/DemoSnippetsPackage.cs
+++ DemoSnippets/DemoSnippetsPackage.cs
@@ -119,6 +119,8 @@ namespace DemoSnippets
 				{
 					{ "VsixVersion", Vsix.Version },
 					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
+					{ "Architecture", RuntimeInformation.ProcessArchitecture.ToString() },
+					{ "MsInternal", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.IsUserMicrosoftInternal.ToString() },
 				};
 
 				client.TrackEvent(Vsix.Name, properties);
